22eed2ccfa23743d7f3854cb395449db824e0d16
Alen <Alendos87@gmail.com>
2024-02-16 22:19:56 +0300

    problems with migrations

--- myapp/models.py
+++ myapp/models.py
@@ -28,17 +28,26 @@ class Review(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     tour = models.ForeignKey(Tour, on_delete=models.CASCADE)
     text = models.TextField()
+    rating = models.IntegerField(choices=[(i, i) for i in range(1, 6)]) # делаем возможность оценить поездку от 1 до 5
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f"{self.tour}"
 
 class Booking(models.Model):
+    STATUS_CHOICES = [
+        ('confirmed', 'Подтверждено'),
+        ('pending', 'Ожидает подтверждения'),
+        ('rejected', 'Отклонено'),
+    ]
+
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     tour = models.ForeignKey(Tour, on_delete=models.CASCADE)
     number_of_people = models.IntegerField()
     phone_number = models.CharField(max_length=20)
-    comments = models.TextField()
+    comments = models.TextField(blank=True)
+    booking_date = models.DateField()
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='pending')
 
     def __str__(self):
-        return f"{self.tour}"
+        return f"{self.tour} - {self.user}"
